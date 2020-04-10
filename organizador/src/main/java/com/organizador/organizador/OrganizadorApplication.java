fa4c8cdb5769056d90a44db9aa2090fb8bb4e0b3
luranoni <lucas.ariel.ranoni@everis.com>
2020-04-10 01:38:02 -0300

    Primer commit

---
+++ organizador/src/main/java/com/organizador/organizador/OrganizadorApplication.java
@@ -0,0 +1,23 @@
+package com.organizador.organizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OrganizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+	
+	
+}
